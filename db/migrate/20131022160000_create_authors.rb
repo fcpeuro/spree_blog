bea97037c80575e567243e93c8bd2a4c59712d81
David Librera <davidlibrera@gmail.com>
2013-10-22 22:53:45 +0200

    removed complete migration and added migration for authors

---
+++ db/migrate/20131022160000_create_authors.rb
@@ -0,0 +1,15 @@
+class CreateAuthors < ActiveRecord::Migration
+
+  def change
+    create_table "spree_authors" do |t|
+      t.string   "first_name"
+      t.string   "last_name"
+      t.text     "bio"
+      t.text     "permalink",  null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+    add_index "spree_authors", ["permalink"],         name: "index_authors_on_permalink"
+  end
+
+end
