e6b43f4d81a39baaa4917e34a9d56cc633c82b81
Denis I <visible@jumph4x.net>
2015-07-15 17:59:56 -0700

    Fix relationships

--- app/models/spree/category.rb
+++ app/models/spree/category.rb
@@ -1,7 +1,8 @@
 module Spree
   class Category < ActiveRecord::Base
     acts_as_nested_set
-    has_many :posts, ->{ order(published_at: :desc) }, inverse_of: :category
+    has_many :post_categories, inverse_of: :category
+    has_many :posts, ->{ order(published_at: :desc) }, through: :post_categories, source: :post
 
     validates :name, :permalink, presence: true
     validates :name, :permalink, uniqueness: { case_sensitive: false }
